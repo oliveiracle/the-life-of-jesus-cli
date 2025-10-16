59392a9f6c7fdf08b4fb1af153559ee773354813
cleinofrank@gmail.com <cleinofrank@gmail.com>
2025-10-16 20:22:52 +0100

    chore: remove bible_verses.py file

--- bible_verses.py
+++
@@ -1,8 +0,0 @@
-# Simple list of Bible verses for random selection
-bible_verses = [
-    "John 3:16 - For God so loved the world...",
-    "Matthew 28:20 - I am with you always...",
-    "Psalm 23:1 - The Lord is my shepherd...",
-    "Romans 8:28 - And we know that in all things God works...",
-    "Philippians 4:13 - I can do all things through Christ...",
-]
